8e7d0dd857a046bb2f846b8eeb7ea32409558301
Steven Bachman <s.bachman@kew.org>
2025-05-15 15:06:21 +0100

    simpler version of redlist analysis with just points rather than EOO, AOO

---
+++ redlist_analysis_basic.R
@@ -0,0 +1,159 @@
+
+# compare previous_data and new occurrence points on a map
+# check for difference in EOO, AOO
+
+# load libraries
+#library(rCAT)
+library(dplyr)
+library(sf)
+library(leaflet)
+library(htmlwidgets)
+
+# step 1 previous data ----
+# read in current data
+#previous_data <- readRDS("data/Pachypodium_rosulatum/previous_data.rds")
+previous_data <- readRDS("data/Trachelophorus_giraffa/previous_data.rds")
+
+# run cleaning protocols
+#flag_errors <- LCr::flag_occs(previous_data)
+#clean_points <- LCr::clean_occs(flag_errors$flagged_data)
+
+clean_points <- previous_data %>%
+  filter(country == "Madagascar")
+
+#reformat points
+thepoints <- clean_points %>%
+  dplyr::select(decimalLatitude, decimalLongitude) %>%
+  dplyr::rename("long" = "decimalLongitude",
+                "lat" = "decimalLatitude") %>%
+  dplyr::filter(complete.cases(lat))
+
+old_points <- thepoints
+
+# project the points for correct area measurements
+#thepoints <- simProjWiz(thepoints)
+#old_points <- simProjWiz(thepoints)
+#old_points_geog <- st_transform(old_points, 4326)
+
+#get the EOO area in km2 for these points
+#eoo_poly <- eoo(thepoints, returnV = "SF")
+#eoo_poly_geog <- st_transform(eoo_poly, 4326) #set to geogrpahic to view on map
+#eoo_val <- eoo(thepoints)
+
+#get the AOO area
+#aoo_val <- aoo(thepoints)
+#aoo_grid <- aoo(thepoints, returnV = "SF", cellsize = 20000)
+#aoo_poly_geog <- st_transform(aoo_grid, 4326) #set to geogrpahic to view on map
+
+# step 2 new data ----
+# do the same for the new data
+#new_recs <- readRDS("data/Pachypodium_rosulatum/new_records_20250515_102643.rds")
+
+# new recs
+files <- list.files("data/Trachelophorus_giraffa",
+                    pattern = "new_records.*\\.rds$",
+                    full.names = TRUE)
+
+df_list <- lapply(files, readRDS)
+new_recs <- do.call(rbind, df_list)
+
+# run cleaning protocols
+#flag_errors <- LCr::flag_occs(new_recs)
+#clean_points <- LCr::clean_occs(flag_errors$flagged_data)
+
+clean_points <- new_recs %>%
+  filter(country == "Madagascar")
+
+#reformat points
+thepoints <- clean_points %>%
+  dplyr::select(decimalLatitude, decimalLongitude) %>%
+  dplyr::rename("long" = "decimalLongitude",
+                "lat" = "decimalLatitude") %>%
+  dplyr::filter(complete.cases(lat))
+
+new_points <- thepoints
+
+# project the points for correct area measurements
+#new_points <- simProjWiz(thepoints)
+#new_points_geog <- st_transform(new_points, 4326)
+
+#get the EOO area in km2 for these points
+#new_eoo_poly <- eoo(thepoints, returnV = "SF")
+#new_eoo_poly_geog <- st_transform(new_eoo_poly, 4326) #set to geogrpahic to view on map
+#new_eoo_val <- eoo(thepoints)
+
+#get the AOO area
+#new_aoo_val <- aoo(thepoints)
+#new_aoo_grid <- aoo(thepoints, returnV = "SF", cellsize = 20000)
+#new_aoo_poly_geog <- st_transform(new_aoo_grid, 4326) #set to geogrpahic to view on map
+
+
+m <- leaflet() %>%
+  leaflet::addProviderTiles("OpenStreetMap", group = "OpenStreetMap") %>%
+  leaflet::addProviderTiles("Esri.WorldImagery", group = "Satellite") %>%
+
+  addCircleMarkers(
+    data = old_points,
+    radius = 5,              # Size of the circles
+    color = "grey",           # Border color
+    weight = 1,              # Border thickness
+    opacity = 1,             # Border opacity
+    fillColor = "grey",       # Fill color
+    fillOpacity = 0.8,       # Fill transparency
+    group = "old points"     # Layer group for toggling
+  ) %>%
+
+  addCircleMarkers(
+    data = new_points,
+    radius = 5,              # Size of the circles
+    color = "green",           # Border color
+    weight = 1,              # Border thickness
+    opacity = 1,             # Border opacity
+    fillColor = "green",       # Fill color
+    fillOpacity = 0.8,       # Fill transparency
+    group = "new points"     # Layer group for toggling
+  ) %>%
+
+  # EOO Polygon
+  # addPolygons(
+  #   data = eoo_poly_geog,
+  #   color = "red",
+  #   weight = 1,
+  #   opacity = 1,
+  #   dashArray = "3",
+  #   fillOpacity = 0,
+  #   group = "EOO"
+  # ) %>%
+
+  # Old AOO Polygon
+  # addPolygons(
+  #   data = aoo_poly_geog,
+  #   fillColor = "red",
+  #   weight = 1,
+  #   opacity = 1,
+  #   color = "black",
+  #   dashArray = "3",
+  #   fillOpacity = 0.3,
+  #   group = "AOO (old)"
+  # ) %>%
+
+  # New AOO Polygon
+  # addPolygons(
+  #   data = new_aoo_poly_geog,
+  #   fillColor = "limegreen",
+  #   color = "darkgreen",
+  #   weight = 3,
+  #   fillOpacity = 1,
+  #   opacity = 1,
+  #   group = "AOO (new)"
+  # ) %>%
+
+  # Layers Control
+  addLayersControl(
+    baseGroups = c("OpenStreetMap", "Satellite"),
+    overlayGroups = c("new points", "old points"),
+    options = layersControlOptions(collapsed = FALSE)
+  )
+m
+
+htmlwidgets::saveWidget(m, file = "map_test.html", selfcontained = TRUE)
